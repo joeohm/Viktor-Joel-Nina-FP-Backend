7035e5913ee9a8da075eed36ba3062f8e41292f4
ViktorSvenssonN <viktor.tommy.svensson@gmail.com>
2022-12-08 14:34:38 +0100

    did codealong to live session

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,14 @@ import mongoose from "mongoose";
 import crypto from "crypto";
 import bcrypt from "bcrypt";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-auth";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8090;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
@@ -138,27 +138,22 @@ const ThoughtSchema = new mongoose.Schema({
 
 const Thought = mongoose.model("Thought", ThoughtSchema);
 
-app.get("/User", authenticateUser);
-app.get("/User", async (req, res) => {
+app.get("/thoughts", authenticateUser);
+app.get("/thoughts", async (req, res)=> {
   const thoughts = await Thought.find({});
-  res.status(200).json({ response: thoughts, success: true });
+  res.status(200).json({success: true, response: thoughts});
 });
 
+app.post("/thoughts", authenticateUser)
 app.post("/thoughts", async (req, res) => {
   const { message } = req.body;
   try {
-    const newThought = await new Thought({ message }).save();
-    res.status(201).json({ response: newThought, success: true });
+    const newThought = await new Thought({message}).save();
+    res.status(201).json({success: true, response: newThought});
   } catch (error) {
-    res.status(400).json({ response: error, success: false });
+    res.status(400).json({success: false, response: error});
   }
 });
-
-app.get("/thoughts", authenticateUser);
-app.get("/thoughts", (req, res)=> {
-  res.status(200).json({success: true, response: "all the thoughts"});
-});
-
 
 ///////
 // Start defining your routes here
---
+++ frontend/src/utils/utils.js
@@ -0,0 +1,3 @@
+const BASE_URL = "https://project-auth-4wtcpuirwa-lz.a.run.app"; // vår api
+
+export const API_URL = (slug) => `${BASE_URL}/${slug}`;
